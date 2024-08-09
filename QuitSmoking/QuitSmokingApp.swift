12b6b7f0242527744a23c662a892ccbd478bec80
ranggaupnvj <2310501014@mahasiswa.upnvj.ac.id>
2024-08-09 17:20:56 +0700

    Initial Commit

---
+++ QuitSmoking/QuitSmokingApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuitSmokingApp.swift
+//  QuitSmoking
+//
+//  Created by Rangga Biner on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct QuitSmokingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
